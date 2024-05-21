c45db3ddb593d892c3a45d620fa5b5c64579071b
awsaunde <awsaunde@iu.edu>
2024-05-21 14:21:45 -0400

    Alex attempt

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
         const x = event.clientX - rect.left - rect.width / 2;
         const y = rect.height / 2 - (event.clientY - rect.top);
 
-        coordinatesDisplay.textContent = `table: (${x.toFixed(2)}, ${y.toFixed(2)})`;
+        const row = document.createElement('tr');
+        const xCell = document.createElement('td');
+        const yCell = document.createElement('td');
+
+        xCell.textContent = x.toFixed(2);
+        yCell.textContent = y.toFixed(2);
+
+        row.appendChild(xCell);
+        row.appendChild(yCell);
+        coordinatesTable.appendChild(row);
     });
 });
 
