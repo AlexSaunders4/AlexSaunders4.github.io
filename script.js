14747b3aa3fef79cbaab9ba53c4200d3ebbfe150
awsaunde <awsaunde@iu.edu>
2024-05-21 14:05:32 -0400

    Hopeful

--- script.js
+++ script.js
@@ -8,18 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const y = rect.height / 2 - (event.clientY - rect.top);
 
         coordinatesDisplay.textContent = `table: (${x.toFixed(2)}, ${y.toFixed(2)})`;
-    });
+    })
 
 
-const row = document.createElement('tr');
-            const xCell = document.createElement('td');
-            const yCell = document.createElement('td');
-
-            xCell.textContent = x;
-            yCell.textContent = y;
-
-            row.appendChild(xCell);
-            row.appendChild(yCell);
-            coordinatesTable.appendChild(row);
-        });
         
